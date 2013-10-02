e3f6665f95daf9925cae5ff6bb2f8d7451ce21da
Floyd Webb <fwebb@itutu.com>
2013-10-02 18:41:38 -0500

    updated

--- js/custom201310.js
+++ js/custom201310.js
@@ -2,9 +2,10 @@ try {
     console.log('we loaded');
 
     $('.wpbdp-listing').each(function(idx, el) {
-        console.log(el);
+        console.log($(el).attr('id'));
         console.log(idx);
     });
+
 } catch(e) {
     console.log('we goofed');
     console.log(e);
