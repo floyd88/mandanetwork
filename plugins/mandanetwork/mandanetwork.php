3c1e874b51fee46124756949753df410eb598ed2
Floyd Webb <fwebb@itutu.com>
2013-10-05 02:56:05 -0500

    added exhibitors query.

--- plugins/mandanetwork/mandanetwork.php
+++ plugins/mandanetwork/mandanetwork.php
@@ -13,14 +13,25 @@
 function update_mandanetwork_user_meta() {
     update_user_meta(get_current_user_id(), 'mandanetwork', $_POST['data']);
     wp_send_json_success();
-    wp_die(0);
 }
 
 /* action = get_mandanetwork_user_meta */
 function get_mandanetwork_user_meta() {
     $data = get_user_meta(get_current_user_id(), 'mandanetwork');
     wp_send_json_success($data);
-    wp_die(0);
+}
+
+/* action = get_mandanetwork_exhibitors */
+function get_mandanetwork_exhibitors() {
+    $ids = explode(',', $_GET['post_ids']);
+    //$posts = get_posts(array('post__in' => $ids));
+    //wp_send_json_success($posts);
+    $html = '';
+    foreach ($ids as $id) {
+        $html .= wpbdp_render_listing($id, 'excerpt') . "\n";
+    }
+    echo $html;
+    wp_die();
 }
 
 function must_login() {
@@ -30,8 +41,10 @@ function must_login() {
 
 add_action("wp_ajax_update_mandanetwork_user_meta", "update_mandanetwork_user_meta");
 add_action("wp_ajax_get_mandanetwork_user_meta", "get_mandanetwork_user_meta");
+add_action("wp_ajax_get_mandanetwork_exhibitors", "get_mandanetwork_exhibitors");
 
 add_action("wp_ajax_nopriv_update_mandanetwork_user_meta", "must_login");
 add_action("wp_ajax_nopriv_get_mandanetwork_user_meta", "must_login");
+add_action("wp_ajax_nopriv_get_mandanetwork_exhibitors", "must_login");
 
 ?>
